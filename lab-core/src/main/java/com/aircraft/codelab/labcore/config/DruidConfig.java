4bdc22f4857ad062a0059a47e26d0dde5026cad0
spike-x <zt_wmail500@163.com>
2021-01-24 01:26:25 +0800

    update

---
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/config/DruidConfig.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020, Tao Zhang (zt_wmail500@163.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aircraft.codelab.labcore.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2020-11-01
+ * Druid配置类
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@Configuration
+@EnableConfigurationProperties(DruidProperties.class)
+public class DruidConfig {
+
+    @Resource
+    private DruidProperties properties;
+
+    @Bean
+    @Primary // 在同样的DataSource中，首先使用被标注的DataSource
+    public DataSource druid() {
+        DruidDataSource dataSource = new DruidDataSource();
+        properties.injectConfig(dataSource);
+        return dataSource;
+    }
+
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> statViewServlet() {
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+        // 控制台管理用户 http://localhost:8080/druid/login.html
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "123456");
+        initParams.put("allow", "127.0.0.1");
+        // IP黑名单(共同存在时，deny优先于allow)
+        initParams.put("deny", "");
+        initParams.put("resetEnable", "false");
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> webStatFilter() {
+        FilterRegistrationBean<WebStatFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.gif,*.jpg,*.png, *.css,*.ico,/druid/*");
+        bean.setInitParameters(initParams);
+        bean.addUrlPatterns("/*");
+        return bean;
+    }
+}
---
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/kafka/MessageConsumer.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020, Tao Zhang (zt_wmail500@163.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aircraft.codelab.labcore.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.Acknowledgment;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 2020-12-06
+ * KafkaConsumer
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+//@Component
+@Slf4j
+public class MessageConsumer {
+    /**
+     * 批量消费
+     *
+     * @param records List<ConsumerRecord>
+     * @param ack     Acknowledgment
+     */
+    @KafkaListener(topicPartitions = {@TopicPartition(topic = "${spring.kafka.template.default-topic}", partitions = {"0", "1", "2"})}, containerFactory = "containerFactory")
+    public void receiveMessage(List<ConsumerRecord<?, ?>> records, Acknowledgment ack) {
+        try {
+            records.forEach(record -> {
+                Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+                if (kafkaMessage.isPresent()) {
+                    log.info("key:{},value:{},partition:{},offset:{}", record.key(), record.value(), record.partition(), record.offset());
+                }
+            });
+        } catch (Exception e) {
+            log.error("Kafka监听异常" + e.getMessage(), e);
+        } finally {
+            ack.acknowledge();
+        }
+    }
+
+    @KafkaListener(topics = {"${spring.kafka.template.default-topic}"})
+    public void receiveMessage(ConsumerRecord<?, ?> record, Acknowledgment ack) {
+        try {
+            Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+            if (kafkaMessage.isPresent()) {
+                log.info("key:{},value:{},partition:{},offset:{}", record.key(), record.value(), record.partition(), record.offset());
+            }
+        } catch (Exception e) {
+            log.error("Kafka监听异常" + e.getMessage(), e);
+        } finally {
+            ack.acknowledge();
+        }
+    }
+}
