dec787a4077f5a039409d06e9f21256968e1cd48
Spike-X <1261402318@qq.com>
2021-09-15 15:10:25 +0800

    Create IMenuService.java

---
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/service/IMenuService.java
@@ -0,0 +1,39 @@
+package com.huawei.pcb.transform.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.huawei.pcb.transform.pojo.entity.SysMenu;
+import com.huawei.pcb.transform.pojo.vo.SysMenuCreatVo;
+import com.huawei.pcb.transform.pojo.vo.SysMenuUpdateVo;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * 2021-08-11
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+public interface IMenuService extends IService<SysMenu> {
+    IPage<SysMenu> query(
+            @NotNull(message = "当前页不能为空")
+            @Max(value = 100000, message = "当前页取值范围1-99999")
+            @Min(value = 1, message = "最小值大于等于1")
+                    int pageNum,
+            @NotNull(message = "每页页数不能为空")
+            @Max(value = 100, message = "每页页数取值范围1-99")
+            @Min(value = 1, message = "最小值大于等于1")
+                    int pageSize,
+            Integer type);
+
+    Long addMenu(SysMenuCreatVo sysMenuCreatVo);
+
+    SysMenu getMenu(String id);
+
+    void updateMenu(SysMenuUpdateVo sysMenuUpdateVo);
+
+    int deleteMenu(List<String> idList);
+}
