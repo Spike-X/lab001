d24010fd94f62aee5b67995ed11650870536ec14
Spike-X <zt_wmail500@163.com>
2021-10-18 13:28:01 +0800

    update

--- lab-core/src/main/java/com/aircraft/codelab/labcore/aop/LogAspect.java
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/aop/LogAspect.java
@@ -1,14 +1,24 @@
 package com.aircraft.codelab.labcore.aop;
 
+import cn.hutool.extra.servlet.ServletUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -20,13 +30,18 @@ import java.util.Optional;
  */
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
     // 抽取公共的切入点表达式
     @Pointcut("execution(* com.aircraft.codelab.labcore.aop.MathCalculator.*(..))")
     public void pointCut() {
     }
 
-//    @Pointcut("execution(* com.aircraft.codelab.labcore.controller.*.*(..))")
+//    @Pointcut("execution(public * com.aircraft.codelab.labcore.controller.*.*(..))")
+//    public void pointCut() {
+//    }
+
+//    @Pointcut("@within(org.springframework.web.bind.annotation.RestController) && execution(public * com.aircraft.codelab..controller..*.*(..))")
 //    public void pointCut() {
 //    }
 
@@ -59,15 +74,48 @@ public class LogAspect {
     }
 
     @Around(value = "pointCut()")
-    public Object surroundInform(ProceedingJoinPoint proceedingJoinPoint) {
+    public Object surroundInform(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("@Around环绕通知开始...");
-        try {
-            Object o = proceedingJoinPoint.proceed();
-            System.out.println("方法环绕proceed，结果是 :" + o);
-            return o;
-        } catch (Throwable e) {
-            e.printStackTrace();
-            return null;
+        LocalDateTime startTime = null;
+        if (log.isDebugEnabled()) {
+            startTime = LocalDateTime.now();
         }
+
+        Object o = proceedingJoinPoint.proceed();
+        if (log.isDebugEnabled()) {
+            LocalDateTime endTime = LocalDateTime.now();
+
+            try {
+                RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+                ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+                HttpServletRequest request = sra.getRequest();
+                LoginInfo loginInfo = WebUtils.getLoginInfoFromRequest(request);
+                String userId = loginInfo == null ? "未登录用户" : loginInfo.getSubject().get("id") + "";
+                String userName = loginInfo == null ? "未登录用户" : loginInfo.getSubject().get("name") + "";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+                log.debug("客户端IP:{}, 调用用户id: {}, 调用用户名: {}, " +
+                                "requestURI: {}, method: {}, 方法参数名: {}, " +
+                                "方法参数: {}, 调用开始时间: {}, 调用结束时间: {}, 时间差(毫秒): {}",
+                        new Object[]{ServletUtil.getClientIP(request), userId, userName, request.getRequestURI(), request.getMethod(),
+                                String.join(",", ((MethodSignature) proceedingJoinPoint.getSignature()).getParameterNames()),
+                                this.methodArgsConvert(proceedingJoinPoint.getArgs()), startTime.format(formatter),
+                                endTime.format(formatter), Duration.between(startTime, endTime).toMillis()});
+            } catch (Exception var12) {
+            }
+        }
+        return o;
+    }
+
+    private String methodArgsConvert(Object[] os) {
+        List<String> list = new ArrayList();
+        Object[] var3 = os;
+        int var4 = os.length;
+
+        for (int var5 = 0; var5 < var4; ++var5) {
+            Object o = var3[var5];
+            list.add(o == null ? "null" : o.toString());
+        }
+
+        return String.join(",", list);
     }
 }
