6a5213b73a1ab318fdc86a40121a7c60a07e3635
Spike-X <1261402318@qq.com>
2021-12-23 10:31:05 +0800

    Add files via upload

---
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/aop/IdempotentAspect.java
@@ -0,0 +1,57 @@
+package com.aircraft.codelab.labcore.aop;
+
+import cn.hutool.crypto.SecureUtil;
+import com.aircraft.codelab.cache.config.RedisConfig;
+import com.aircraft.codelab.core.entities.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 2021-12-22
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@Aspect
+@Component
+@Slf4j
+@ConditionalOnClass(RedisConfig.class)
+public class IdempotentAspect {
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Around("@annotation(idempotent)")
+    public Object noRepeatSubmit(ProceedingJoinPoint proceedingJoinPoint, Idempotent idempotent) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+        // 生产使用请求头token作为id
+        String sessionId = Objects.requireNonNull(attributes).getSessionId();
+        log.debug("===" + sessionId);
+        String requestURI = request.getRequestURI();
+        log.debug("requestURI: {}", requestURI);
+        String key = SecureUtil.md5(sessionId + requestURI);
+        log.debug("key: {}", key);
+        String redisKey = String.format("idempotent:%s", key);
+        Boolean success = setIfAbsent(redisKey, idempotent.timeout(), idempotent.timeUnit());
+        if (success) {
+            return proceedingJoinPoint.proceed();
+        }
+        return CommonResult.success(idempotent.message());
+    }
+
+    public Boolean setIfAbsent(String key, long timeout, TimeUnit timeUnit) {
+        return redisTemplate.opsForValue().setIfAbsent(key, "", timeout, timeUnit);
+    }
+}
