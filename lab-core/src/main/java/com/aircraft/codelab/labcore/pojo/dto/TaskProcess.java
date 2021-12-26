848e78ad47df17537b931db95ffbf7e8a4722257
Spike-X <zt_wmail500@163.com>
2021-12-27 00:40:51 +0800

    update

---
+++ lab-core/src/main/java/com/aircraft/codelab/labcore/pojo/dto/TaskProcess.java
@@ -0,0 +1,37 @@
+package com.aircraft.codelab.labcore.pojo.dto;
+
+import com.aircraft.codelab.labcore.enums.TaskState;
+import lombok.Data;
+
+/**
+ * 2021-12-26
+ *
+ * @author tao.zhang
+ * @since 1.0
+ */
+@Data
+public class TaskProcess {
+    private TaskState state = TaskState.START;
+
+    public void nextState() {
+        this.state = this.state.nextState();
+    }
+
+    public void log() {
+        System.out.println(this.state.prevState() + "--->" + this.state.name());
+    }
+
+    public static void main(String[] args) {
+        TaskProcess taskProcess = new TaskProcess();
+//        TaskState instance = TaskState.getInstance(1001);
+//        TaskState taskState = instance.nextState();
+//        taskProcess.setState(instance);
+
+        taskProcess.nextState();
+        taskProcess.log();
+        taskProcess.nextState();
+        taskProcess.log();
+
+        System.out.println(taskProcess.state.prevState().name());
+    }
+}
