8d5373dc0b0272342f84f5552b6f1444c0dab499
Spike-X <zt_wmail500@163.com>
2022-01-18 00:52:52 +0800

    update

--- common-cache/src/main/java/com/aircraft/codelab/cache/config/RedisConfig.java
+++ common-cache/src/main/java/com/aircraft/codelab/cache/config/RedisConfig.java
@@ -6,16 +6,11 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -35,7 +30,7 @@ import java.time.Duration;
 //@EnableCaching
 @Configuration
 public class RedisConfig {
-        @Bean
+    @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
@@ -62,7 +57,7 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager redisCacheManager(LettuceConnectionFactory redisConnectionFactory) {
-        RedisSerializer<String > redisSerializer = new StringRedisSerializer();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofDays(1))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
