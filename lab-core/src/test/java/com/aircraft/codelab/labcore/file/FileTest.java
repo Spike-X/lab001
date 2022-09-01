bb286fc965a9d53a4d5cd1acb045c80ccba8494c
tao <zt_wmail500@163.com>
2022-09-02 00:22:00 +0800

    update

--- lab-core/src/test/java/com/aircraft/codelab/labcore/file/FileTest.java
+++ lab-core/src/test/java/com/aircraft/codelab/labcore/file/FileTest.java
@@ -1,8 +1,21 @@
 package com.aircraft.codelab.labcore.file;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.extra.emoji.EmojiUtil;
 import lombok.extern.slf4j.Slf4j;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 2022-09-01
@@ -14,7 +27,70 @@ import org.junit.jupiter.api.Test;
 public class FileTest {
     @Test
     @DisplayName("文件测试")
-    void fileTest() {
+    void fileTest() throws ZipException {
+        File file = FileUtil.mkdir("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp");
+        // 仅创建目录
+        File file1 = FileUtil.mkdir("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp\\filename.zip");
 
+        String mimeType = FileUtil.getMimeType("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp\\IMG_0678.JPG");
+        log.debug("mimeType: {}", mimeType);
+
+        // IMG_0678
+        String filename = FileUtil.mainName("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp\\IMG_0678.JPG");
+        // JPG
+        String extname = FileUtil.extName("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp\\IMG_0678.JPG");
+        log.debug("filename: {}, extname: {}", filename, extname);
+
+        File file2 = new File("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp");
+        // 遍历目录下的所有文件和目录，递归计算其大小
+        long size = FileUtil.size(file2);
+        String filesize = NumberUtil.decimalFormatMoney(size / 1024.00 / 1024.00);
+        log.debug("filesize: {}(MB)", filesize);
+
+        // 获得指定目录下所有文件的文件名
+        List<String> fileNames = FileUtil.listFileNames("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp");
+        log.debug("fileNames: {}", fileNames);
+
+        // 列出当前目录下的目录和文件路径
+        File[] files = FileUtil.ls("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp");
+        File[] files1 = new File("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp").listFiles();
+        // 筛选出是文件的路径
+        List<String> filePath = Arrays.stream(files).map(f -> {
+            try {
+                if (f.isFile()) {
+                    return f.getCanonicalPath();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+        log.debug("filePath1: {}", filePath);
+
+        List<File> fileList = FileUtil.loopFiles("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp");
+        List<File> fileList1 = FileUtil.loopFiles("D:\\Users\\zt_wm\\Downloads\\aTemp\\bTemp", f -> f.getName().endsWith("txt"));
+        List<String> list = fileList.stream().map(f -> {
+            try {
+                return f.getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+        log.debug("filePath2: {}", list);
+
+//        ZipFile zipFile = new ZipFile("D:\\Users\\zt_wm\\Downloads\\aTemp\\filename.zip");
+//        // 10M = 10 * 1024 * 1024 = 10485760B
+//        zipFile.createSplitZipFile(fileList, new ZipParameters(), true, 2097152);
+
+        String str = "An 😀awesome 😃string with a few 😉emojis!";
+        log.debug(str);
+        if (EmojiUtil.containsEmoji(str)) {
+            String toHtml = EmojiUtil.toHtml(str);
+            // An &#x1f600;awesome &#x1f603;string with a few &#x1f609;emojis!
+            log.debug(toHtml);
+            String unicode = EmojiUtil.toUnicode(toHtml);
+            log.debug(unicode);
+        }
     }
 }
