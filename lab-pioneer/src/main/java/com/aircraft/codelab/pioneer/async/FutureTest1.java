3388664cdff326281c17c8395611402f42fa0aba
tao <zt_wmail500@163.com>
2023-12-15 02:09:25 +0800

    CompletableFuture 异常

--- lab-pioneer/src/main/java/com/aircraft/codelab/pioneer/async/FutureTest1.java
+++ lab-pioneer/src/main/java/com/aircraft/codelab/pioneer/async/FutureTest1.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Locale;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -76,7 +77,7 @@ public class FutureTest1 {
 
 
     public static void main(String[] args) {
-        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+        /*CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             // Some computation
             return "Hello";
         });
@@ -91,7 +92,56 @@ public class FutureTest1 {
             return __ + " " + f2;
         });
 
-        thenCombine.thenAccept(System.out::println);
+        thenCombine.thenAccept(System.out::println);*/
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            Thread thread1 = Thread.currentThread();
+            log.debug("thread1: {}", thread1.getName() + "_" + thread1.getId());
+            // 执行耗时操作
+//                int a = 1/0;
+            return "result1";
+        });
+
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
+            Thread thread2 = Thread.currentThread();
+            log.debug("thread2: {}", thread2.getName() + "_" + thread2.getId());
+            // 执行耗时操作
+            try {
+//                int a = 1/0;
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
+            }
+            return 42;
+        });
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(future1, future2);
+        Thread thread3 = Thread.currentThread();
+        log.debug("thread3: {}", thread3.getName() + "_" + thread3.getId());
+        CompletableFuture<Void> finalResult = allFutures.thenComposeAsync(voidResult -> {
+            Thread thread4 = Thread.currentThread();
+            log.debug("thread4: {}", thread4.getName() + "_" + thread4.getId());
+            if (!future1.isCompletedExceptionally() && !future2.isCompletedExceptionally()) {
+                return CompletableFuture.completedFuture(null)
+                        .thenAccept(ignored -> {
+                            try {
+                                String result1 = future1.get();
+                                Integer result2 = future2.get();
+                                log.debug("result1: {}, result2: {}", result1, result2);
+                                Thread thread5 = Thread.currentThread();
+                                log.debug("thread5: {}", thread5.getName() + "_" + thread5.getId());
+                                // 执行操作处理返回值
+                            } catch (InterruptedException | ExecutionException e) {
+                                log.error(e.getMessage(), e);
+                                // 处理获取返回值时的异常情况
+                            }
+                        });
+            } else {
+                return CompletableFuture.completedFuture(null);
+            }
+        });
+        Thread thread6 = Thread.currentThread();
+        log.debug("thread6: {}", thread6.getName() + "_" + thread6.getId());
+        finalResult.join(); // 等待所有操作完成
     }
 
 
